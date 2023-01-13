3faae2ac30a5c5a5f7484e528bc33b49b861998a
Stephanie <stephanieyaur2022@u.northwestern.edu>
2023-01-12 21:21:57 -0600

    Changed app title

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-// import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './Home.js';
@@ -17,22 +16,6 @@ import Niantic from './Projects/Niantic';
 
 const App = () => {
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
     <div className="App">
       <div id="fillerNavBar"></div>
       <BrowserRouter basename={process.env.PUBLIC_URL}>
--- src/Projects/Amex.js
+++ src/Projects/Amex.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import NavBar from "../NavBar";
 import projectsData from "../projectsData";
 
